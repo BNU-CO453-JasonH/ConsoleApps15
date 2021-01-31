447ea8bdbf211475786d1c6a26429c7207b37c4b
JHuggins11 <58664223+JHuggins11@users.noreply.github.com>
2021-01-31 20:42:10 +0000

    Created initial attributes, methods and comments.

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -1,13 +1,47 @@
 ﻿namespace ConsoleAppProject.App01
 {
     /// <summary>
-    /// Please describe the main features of this App
+    /// This App converts a user-inputted distance in miles into its distance in feet, displaying the result to the user afterwards.
     /// </summary>
     /// <author>
-    /// Student Name version 0.1
+    /// Jason Huggins (modified 31/01/2021)
     /// </author>
     public class DistanceConverter
     {
+        private double miles;
+        private double feet;
 
+        /// <summary>
+        /// Runs the application.
+        /// </summary>
+        public void Run()
+        {
+
+        }
+
+        /// <summary>
+        /// Prompt the user to input the distance in miles (as a double number).
+        /// </summary>
+        private void InputMiles()
+        { 
+
+        }
+
+        /// <summary>
+        /// Calculates the conversion of the distance in miles (inputted by the user) into the distance in feet.
+        /// </summary>
+        private void CalculateFeet()
+        {
+
+        }
+
+
+        /// <summary>
+        /// Outputs the conversion result (the distance in feet) to the user.
+        /// </summary>
+        private void OutputMiles()
+        {
+
+        }
     }
 }
