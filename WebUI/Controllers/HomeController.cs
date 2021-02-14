b5de70fbf912831a0db27afae0646ccefd216a43
JHuggins11 <58664223+JHuggins11@users.noreply.github.com>
2021-02-14 23:24:21 +0000

    Following the first web UI video

--- WebUI/Controllers/HomeController.cs
+++ WebUI/Controllers/HomeController.cs
@@ -11,14 +11,17 @@ namespace WebUI.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        public IActionResult Index()
         {
-            _logger = logger;
+            return View();
         }
 
-        public IActionResult Index()
+        public IActionResult DistanceConverter()
+        {
+            return View();
+        }
+
+        public IActionResult BMICalculator()
         {
             return View();
         }
